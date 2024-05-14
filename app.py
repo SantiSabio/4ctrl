2aef4e93a438eb826e7c523cb5810c1a369ff946
SantiSabio <santiagomartinsabio@gmail.com>
2024-05-14 18:10:54 -0300

    agregar db

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from routes.products import products
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+SQLAlchemy(app)
 
 app.register_blueprint(products)
