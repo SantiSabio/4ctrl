e17f9429a0b87df0e3aa2bff3f9d215d8ebbc210
SantiSabio <138256848+SantiSabio@users.noreply.github.com>
2024-05-15 14:41:21 -0300

    casi listo

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask
 from routes.products import products
-from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://santi:password@localhost/productos'
-
-SQLAlchemy(app)
 
 app.register_blueprint(products)
