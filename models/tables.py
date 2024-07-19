ed9875acfd1911ceb475127858740b0cff3811f0
SantiSabio <santiagomartinsabio@gmail.com>
2024-07-18 23:06:11 -0300

    try

--- models/tables.py
+++ models/tables.py
@@ -4,12 +4,8 @@ class Productos(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
     precio = db.Column(db.Float, nullable=False)
-<<<<<<< HEAD
-    marca = db.Column(db.Integer, db.ForeignKey('marca.nombre'), nullable=False)
-
-=======
     marca = db.Column(db.Integer, db.ForeignKey('marcas.nombre'), nullable=False)
->>>>>>> ac2ab107bc3f13397e9a6cb474bb71aa13b1b29e
+
     def __repr__(self):
         return f"Product('{self.nombre}', '{self.marca}', '{self.precio}')"
     
